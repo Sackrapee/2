099e697ef4b3df3c06b9d242a49119bd4c1dcce4
Sackrapee <120537870+Sackrapee@users.noreply.github.com>
2022-12-21 12:00:23 +0700

    Add files via upload

---
+++ JavaScript/DataTypes.js
@@ -0,0 +1,11 @@
+const people = ["Aaron","Mel","John"];
+const one = 1;
+const str = "Hello World" ;
+const b = ture;
+const employee = {
+    firstName: "Anirach"
+    lastName: "Mingkhwan"
+
+};
+
+function sayHello()
